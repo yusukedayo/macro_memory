769a4335c384b41161e65f211c72dc83c48c04a9
yusukedayo <tsunemi0217@icloud.com>
2022-05-20 01:00:42 +0900

    Add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  resources :questions
+  resources :questions do
+    resources :answers, only: %i[create edit destroy update]
+  end
 end
