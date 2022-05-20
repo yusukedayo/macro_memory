616eb193c00f9f9e324aa212ecec0f3cc7b0f733
yusukedayo <tsunemi0217@icloud.com>
2022-05-20 14:36:32 +0900

    Add: 判定機能実装のためにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_152755) do
+ActiveRecord::Schema.define(version: 2022_05_20_045351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_05_19_152755) do
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "correct_rate"
+    t.string "forgotten_words"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
@@ -33,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_05_19_152755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "correct_answer", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
